5ab8a484a30c8d34550b46daf19c5e5398a38ffc
Andrew Bergheim <andrewbergheim@gmail.com>
2019-12-09 21:19:23 -0700

    Character filtering

--- assets/js/UDapi.js
+++ assets/js/UDapi.js
@@ -1,25 +1,55 @@
 
 $("#search-word").on("click",function() {
     //get search term
-    searchTerm = $("#").val()
-    queryURL = "http://api.urbandictionary.com/v0/define?term=" + searchTerm;
+    let searchTerm = $("#").val()
+    let queryURL = "http://api.urbandictionary.com/v0/define?term=" + searchTerm;
 
     $.ajax({
         url:queryURL,
         method: "GET"
     }).then(function(response){
-            section = $("<div>");
+            let section = $("<div>");
             // defining the tags for each section
-            wordTag = $("<h3>").attr("id", "word")
-            definitionTag = $("<h5>").attr("id", "definition")
-            exampleTag = $("<h6>").attr("id", "example")
+            let wordTag = $("<h3>").attr("id", "word")
+            let definitionTag = $("<h5>").attr("id", "definition")
+            let exampleTag = $("<h6>").attr("id", "example")
             
 
             // variables for tag text
-            word = response[0].word;
-            definition = response[0].definition;
-            example = response[0].example;
+            let wordRaw = response[0].word;
+            let definitionRaw = response[0].definition;
+            let exampleRaw = response[0].example;
            
+            //remove invalid characters from each string
+
+            let wordArray = wordRaw.split()
+            for (let i = wordArray.length-1; i > -1; i--){
+                if (wordArray[i] == "[" || wordArray[i] == "]"){
+                    wordArray.splice(i)
+                }
+            }
+            let word = wordArray.toString()
+
+            let definitionArray = definitionRaw.split()
+            for (let i = definitionArray.length-1; i > -1; i--){
+                if (definitionArray[i] == "[" || definitionArray[i] == "]"){
+                    definitionArray.splice(i)
+                }
+            }
+
+            definition = definitionArray.toString()
+
+            let exampleArray = exampleRaw.split()
+            for (let i = exampleArray.length-1; i > -1; i--){
+                if (exampleArray[i] == "[" || exampleArray[i] == "]"){
+                    exampleArray.splice(i)
+                }
+            }
+
+            let example = exampleArray.toString()
+
+
+
             //adding the text to the tags
             wordTag.text(word)
             definitionTag.text(definition)
